001436e35ed250ed3bcdfd280d3d00c0ed6b0268
Rade58 <bajic.rade2@gmail.com>
2021-03-04 20:36:46 +0100

    👾

--- name_lib.py
+++ name_lib.py
@@ -2,8 +2,15 @@ def name_to_uppercase(name="john"):
     return name.upper()
 
 
-print("Kevin Kevanson")
-print(name_to_uppercase())
+# EVO DOLE JE TAJ CODE KOJI SE IZVRSAVA U OVOM GLOBALNOM OBIMU
 
-print(f"dunnder name: {__name__}")  # OVO SE STAMPALO
-#                                   dunnder name: __main__
+# OVIM MOGU DA OSIGURAM DA SE ON NE IZVRSAVA AKO SE FILE KORISTI KAO MODULE
+
+if __name__ == "__main__":
+
+    # OVDE TRBA DA SE NAPRAVI INDENTATION JER OVO TREBA A BUDE U
+    # USLOVNOJ IZJAVI
+    print("Kevin Kevanson")
+    print(name_to_uppercase())
+
+    print(f"dunnder name: {__name__}")
