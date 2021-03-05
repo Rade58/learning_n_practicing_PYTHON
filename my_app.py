fa7b83e1ad742e0f883a225f9fdfa2b269b5dc50
Rade58 <bajic.rade2@gmail.com>
2021-03-05 22:32:59 +0100

    👾

--- my_app.py
+++ my_app.py
@@ -11,22 +11,22 @@ def create_query(languages, min_stars=48000):
     return query
 
 
-# EVO DODAJEM POMENUTE PARAMETRE
-
 def repoi_sa_najvise_starova(query, sort="stars", order="desc"):
     gh_api_url = "https://api.github.com/search/repositories"
 
-    # I OVDE CE SE SADA KORISTITI TI PARAMETRI
     params = {"q": query, "page": 1, "per_page": 8,
               "order": order, "sort": sort}
 
     response = requests.get(gh_api_url, params=params)
 
+    # OVDE CU DA PROVERIM STATUS CODE
+    if response.status_code != 200:
+        # I OVDE CU RAISE-OVATI RUNTIME ERROR
+        raise RuntimeError("An error occured!")
+
     response_json = response.json()
 
     return response_json["items"]
-
-# ---------------------------------------------------------
 
 
 if __name__ == "__main__":
