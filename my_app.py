7a18c6d48212ac104bc7906ee50e656cfdeac998
Rade58 <bajic.rade2@gmail.com>
2021-03-05 16:39:55 +0100

    👾

---
+++ my_app.py
@@ -0,0 +1,11 @@
+import requests
+
+# ZELIM A RUNN-UJEM CODE IN THE __main__ METHOD
+
+# ALI ZA SDA KORISTIM NESTO ZA STA NISAM CUO DA SAD
+# TO JE PYTHON CONSTRUCT, KOJ ISE ZOVE pass
+
+# DAKLE TO JE pass KEYWORD
+
+if __name__ == "__main__":
+    pass
