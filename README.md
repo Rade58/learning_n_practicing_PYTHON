3421861a6d46bcc994c18f37afc18c25b52eaed3
Rade58 <bajic.rade2@gmail.com>
2021-03-05 22:19:12 +0100

    👾

--- README.md
+++ README.md
@@ -329,3 +329,51 @@ requests --> Python --> 44709
 scrapy --> Python --> 39968
 transformers --> Python --> 41836
 ```
+
+MOGAO SAM MALO POPRAVITI FUNKCIJU KAKO BI SE MOZDA MOGLI PASS-OVATI IN I SORT I ORDER
+
+- `code my_app.py`
+
+```py
+import requests
+
+
+def create_query(languages, min_stars=48000):
+
+    query = f"stars:>{min_stars} "
+
+    for language in languages:
+        query += f"language:{language}"
+
+    return query
+
+
+# EVO DODAJEM POMENUTE PARAMETRE
+
+def repoi_sa_najvise_starova(query, sort="stars", order="desc"):
+    gh_api_url = "https://api.github.com/search/repositories"
+
+    # I OVDE CE SE SADA KORISTITI TI PARAMETRI
+    params = {"q": query, "page": 1, "per_page": 8,
+              "order": order, "sort": sort}
+
+    response = requests.get(gh_api_url, params=params)
+
+    response_json = response.json()
+
+    return response_json["items"]
+
+# ---------------------------------------------------------
+
+
+if __name__ == "__main__":
+
+    items = repoi_sa_najvise_starova(
+        create_query(["Python"], 38000))
+
+    for item in items:
+        current = f"{item['name']} --> {item['language']} --> {item['stargazers_count']}"
+        print(current)
+
+```
+
--- my_app.py
+++ my_app.py
@@ -1,36 +1,24 @@
 import requests
 
-# PRAVICU OVDE QUERY (TO JE ONO STO TREBA DA BUDE VREDNOST q PARAMS-A)
-# U OVOJ NOVOJ FUNKCIJI
-
-# FUNKCIJA CE UZIMATI LISTU LANGUAGE-OVA I MINIMALNI BROJ STAR-OVA
-
 
 def create_query(languages, min_stars=48000):
-    # NA KRAJU STRINGA JE EMOPTY SPACE JER TAKO TO GITHUB OCEKUJE
+
     query = f"stars:>{min_stars} "
 
-    # LOOP-UJEM THROUGH languages DA BIH APPENDOVAO SVE TE ZELJENE JEZIKE
-    # NA QUERY
     for language in languages:
         query += f"language:{language}"
 
     return query
 
-# FUNKCIJU REDEFINISEM A UZIMA QUERY
 
+# EVO DODAJEM POMENUTE PARAMETRE
 
-def repoi_sa_najvise_starova(query):
+def repoi_sa_najvise_starova(query, sort="stars", order="desc"):
     gh_api_url = "https://api.github.com/search/repositories"
 
-    # OVDE SAD UMESTO OVOGA
-    # params = {"q": "stars:>48000", "page": 1, "per_page": 8}
-    # KORISTIM PASSED IN QUERY
-    # # UZ TO CU DODATI DA SE SORT-UJU PREMA BROJU FORKOVA
-    # I DA ORDER BUDE ASCENDING
-
+    # I OVDE CE SE SADA KORISTITI TI PARAMETRI
     params = {"q": query, "page": 1, "per_page": 8,
-              "order": "asc", "sort": "forks"}
+              "order": order, "sort": sort}
 
     response = requests.get(gh_api_url, params=params)
 
@@ -42,8 +30,7 @@ def repoi_sa_najvise_starova(query):
 
 
 if __name__ == "__main__":
-    # pass
-    # OVDE SAD FUNKCIJI MOZES DATI ZELJENI QUERY
+
     items = repoi_sa_najvise_starova(
         create_query(["Python"], 38000))
 
